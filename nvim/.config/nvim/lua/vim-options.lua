5cd8bfab0617060622a53d5bb1e6f1ab8854a93b
Nam Nguyen <mznnht@amazon.com>
2024-09-08 11:28:38 -0700

    Update files indentation

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,12 +6,15 @@ vim.g.mapleader = " "
 vim.g.background = "light"
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- Noice
+vim.api.nvim_set_keymap("n", "<leader>nn", ":Noice dimiss<CR>", { noremap = true })
 
 vim.opt.swapfile = false
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
